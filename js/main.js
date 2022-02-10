b8df951c077b8360aa07824cc9274979dbd3c1eb
Vinicius Fernandes <vinicius.turu@gmail.com>
2022-02-10 18:10:12 -0300

    Ajustes no layout e finalização da descrição

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// function carregaCatalogo() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             carregaXml(this);
+//         }
+//     };
+//     xhr.open('GET' , './js/xml/empresas.xml' , true);
+//     xhr.send();
+// }
+
+// function carregaXml(xml) {
+//     var docXML = xml.responseXML;
+//     var card = "";
+//     var empresas = docXML.getElementsByTagName('empresa');
+//     for (var i = 0; i<empresas.length; i++) {
+//         card += "<div class='item'><img src='";
+//         card += empresas[i].getElementsByTagName('imagem')[0].textContent;
+//         card += "' onclick='carregaCatalogo();'></div>";
+//     }
+
+//     document.getElementById('manufatura').innerHTML = card;
+// }
+
+// carregaCatalogo();
+
 var item = document.getElementsByClassName('item');
 
 console.log(item);
@@ -28,12 +54,12 @@ function criarDescrição () {
     
     div.style.display = 'block';
     
-    var p = document.createElement('pt');
-    p.textContent = 'teste';
-    p.style.cssText = 
-    'color: #fff;' + 
-    'backgroud-color: yellow;';
-    div.appendChild(p);
+    // var p = document.createElement('pt');
+    // p.textContent = 'teste';
+    // p.style.cssText = 
+    // 'color: #fff;' + 
+    // 'backgroud-color: yellow;';
+    // div.appendChild(p);
 
     content.style.display = 'none';
 
@@ -49,4 +75,3 @@ for (var i = 0; i < item.length; i++) {
         
     })
 }
-
